3877b97f55e2855d02ed1e8920d4050132f28d8e
Michael Harmon <mfh.scoobydoo@gmail.com>
2026-02-03 15:32:41 -0700

    split out pin logic

--- src/yrig/spline/matrix_spline/__init__.py
+++ src/yrig/spline/matrix_spline/__init__.py
@@ -2,8 +2,8 @@ from .core import (
     MatrixSpline,
     bound_curve_from_matrix_spline,
     closest_point_on_matrix_spline,
-    pin_to_matrix_spline,
 )
+from .pin import pin_to_matrix_spline
 
 __all__ = [
     "MatrixSpline",
