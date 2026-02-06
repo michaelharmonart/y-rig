7330bd2fc4d49dc205ec78968af5c09bab522ae3
Michael Harmon <mfh.scoobydoo@gmail.com>
2026-02-06 13:31:11 -0700

    update spline organization and add build function

--- src/yrig/spline/matrix_spline/__init__.py
+++ src/yrig/spline/matrix_spline/__init__.py
@@ -1,13 +1,17 @@
+from . import build, core, pin
 from .core import (
     MatrixSpline,
     bound_curve_from_matrix_spline,
-    closest_point_on_matrix_spline,
+    closest_parameter_on_matrix_spline,
 )
 from .pin import pin_to_matrix_spline
 
 __all__ = [
+    "build",
+    "core",
+    "pin",
     "MatrixSpline",
     "bound_curve_from_matrix_spline",
-    "closest_point_on_matrix_spline",
+    "closest_parameter_on_matrix_spline",
     "pin_to_matrix_spline",
 ]
