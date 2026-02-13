e3eae2760606877b62a6f0fec002a5c432239994
Michael Harmon <mfh.scoobydoo@gmail.com>
2026-02-12 19:02:42 -0700

    update setup script

--- maya-setup.py
+++ maya-setup.py
@@ -5,12 +5,12 @@ from pathlib import Path
 dev_path = Path("~/maya-python/y-rig").expanduser() # Replace with the path to your y-rig repo
 yrig_path = (dev_path / Path("src")).resolve()
 venv_path = (dev_path / Path(".venv/lib/python3.11/site-packages")).resolve()
-component_path = (dev_path / Path("shifter/components")).resolve()
 if str(yrig_path) not in sys.path:
     sys.path.insert(0, str(yrig_path))
 if str(venv_path) not in sys.path:
     sys.path.insert(0, str(venv_path))
 import yrig
+component_path = (dev_path / Path("shifter/components")).resolve()
 os.environ["MGEAR_SHIFTER_COMPONENT_PATH"] = str(component_path)
 
 # Run to reload yrig during development
