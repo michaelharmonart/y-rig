3f27826ca52d069d38c529f4d374bffd14a621a2
Michael Harmon <mfh.scoobydoo@gmail.com>
2026-09-16 12:29:59 -0600

    add base psd setup, and import for hand psd

--- src/yrig/deformer/blendshape/__init__.py
+++ src/yrig/deformer/blendshape/__init__.py
@@ -1,5 +1,6 @@
 from . import core
 from .core import (
+    create_blendshape,
     export_maya_shape_file,
     get_target_weights,
     import_maya_shape_file,
@@ -10,6 +11,7 @@ from .serialize import export_blendshape, get_blendshape_data, import_blendshape
 __all__ = [
     "build_blendshape_networks",
     "core",
+    "create_blendshape",
     "export_blendshape",
     "export_maya_shape_file",
     "get_blendshape_data",
